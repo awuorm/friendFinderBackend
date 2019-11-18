f84cd8cc5a5414413c6b448d0922ea38fd881209
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-11-18 13:02:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const server = require("./server");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT;
+
+server.get("*",(req,res) => {
+    res.status(200).json("Hello from friend's finder app");
+})
+
+server.listen(port,() => {
+    console.log("listening on port",port);
+})
